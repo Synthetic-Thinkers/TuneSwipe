4bfdcb40a6ad9e162752ad2f8bfc833da57ad8c9
Desire Hernandez <92909243+DesireHernandez@users.noreply.github.com>
2025-03-06 22:17:44 -0800

    Revert "Login Token Merge"

--- supabase.js
+++
@@ -1,5 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_DOMAIN
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_SECRET
-export const supabase = createClient(supabaseUrl, supabaseKey)
