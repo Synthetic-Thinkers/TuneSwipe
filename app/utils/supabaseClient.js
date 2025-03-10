244c33eb2130f7d18c3c4b9d25081ac77d0ea704
Jackie Li <jli522543@gmail.com>
2025-03-09 22:55:26 -0700

    TS-121 Updated configs

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your own Supabase URL and API key
-const SUPABASE_URL = process.env.EXPO_PUBLIC_URL
-const SUPABASE_KEY = process.env.EXPO_PUBLIC_KEY
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_DOMAIN
+const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_SECRET
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabase;
