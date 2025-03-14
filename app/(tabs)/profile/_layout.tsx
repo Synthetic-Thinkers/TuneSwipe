16d5c4f1e87f674c1a2c38a5305f40737a05ecbe
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2025-03-13 22:49:17 -0700

    Removed duplicates from package-lock.json, updated package.json, and resolved conflict error.

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
+import { MenuProvider } from 'react-native-popup-menu';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="likedSongScreen" options={{ headerShown: false }} />
-    </Stack>
+    <MenuProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="likedSongScreen" options={{ headerShown: false }} />
+      </Stack>
+    </MenuProvider>
   );
 }
