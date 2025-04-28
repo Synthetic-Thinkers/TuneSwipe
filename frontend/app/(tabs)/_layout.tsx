9f06c8180b1829d706d849dd9f98da5f649fd541
tnaik <98431300+tnaik@users.noreply.github.com>
2025-04-27 23:03:08 -0700

    added listen

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -47,7 +47,7 @@ export default function TabLayout() {
           }}
         />
         <Tabs.Screen
-          name='index'
+          name='listen'
           options={{
             headerTitle: () => <CustomerHeader />,
             title: 'Listen',
